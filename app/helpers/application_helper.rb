ccf775222b59576a36fdf356bb74400300328823
Active Developer <developerinaction@yahoo.de>
2015-12-09 22:57:18 +0000

    Home, About and Directions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+	def get_season()
+		time = Time.new
+
+		if (time.month >= 3) && (time.month <= 5)
+			"Yeah it is spring"
+		elsif (time.month > 5) && (time.month <= 8)
+			"Everyone loves summer"
+		elsif (time.month > 8) && (time.month <= 10)
+			"Leaves are already falling"
+		else
+			"It is freezing"
+		end
+	end
+
+	def get_random_number(max_value = 10)
+		rand(max_value)
+	end
+
+	def get_random_welcome()
+		opening = ["What a beautiful day.","Welcome to our site.","Thank you for stopping by."]
+		middle = ["We hope you find what you need.", "We have a wide selection.", "Check out our items on Sale."]
+		ending = ["Contact us if you need help.", "Thank You for visiting our site.", "In case of any query, please feel free to contact us."]
+
+		"#{opening[rand(3)]} #{middle[rand(3)]} #{ending[rand(3)]}"
+	end
 end
